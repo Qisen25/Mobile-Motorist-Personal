0eee84a356ab4e9c9cf2d6743b20899cfb48e90d
Damon Ezard <damonezard@gmail.com>
2020-08-17 12:17:35 +0800

    Convert back to JavaScript

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,6 +1,7 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+
+const App = require("../App.js").default
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
