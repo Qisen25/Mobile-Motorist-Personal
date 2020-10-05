6e8a839e1ae84de6219908e43298ceca08bb87f6
jaronsi <69338191+jaronsi@users.noreply.github.com>
2020-10-05 20:21:58 +0800

    Update RouteRetriever.js
    Edited the request url.

--- src/utils/RouteRetriever.js
+++ src/utils/RouteRetriever.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const routeRetriever = async function(start, end) {
     const address_1 = "https://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=";
-    const address_2 = "%2Cwa&wp.1=";
-    const address_3 = "%2Cwa&key=AmuFbtSSx6QgGZHVVfruUMH7l49P29nXwK4VbU9fzKX9-QXEdaWjT8gtfuMe7G70&routeAttributes=routePath"
+    const address_2 = "&wp.1=";
+    const address_3 = "&key=AmuFbtSSx6QgGZHVVfruUMH7l49P29nXwK4VbU9fzKX9-QXEdaWjT8gtfuMe7G70&routeAttributes=routePath"
     const address_full = address_1 + start + address_2 + end + address_3;
     
     try {
